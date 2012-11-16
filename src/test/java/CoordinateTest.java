38b583430152ad5d81be4af210c14f87bcf90378
Tyson Gern <tygern@gmail.com>
2012-11-16 09:20:43 -0700

    Add test for Coordinate

---
+++ src/test/java/CoordinateTest.java
@@ -0,0 +1,50 @@
+import org.junit.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CoordinateTest{
+
+    Coordinate pointA;
+    Coordinate pointB;
+    Coordinate pointC;
+    Coordinate pointD;
+
+    @Before public void setUp() {
+        pointA = new Coordinate(1,2);
+        pointB = new Coordinate(0,-4);
+        pointC = new Coordinate(1,3);
+        pointD = new Coordinate(1,2);
+    }
+    
+    @After public void tearDown() {
+        pointA = null;
+        pointB = null;
+        pointC = null;
+        pointD = null;
+    }
+    
+    @Test
+    public void testEquals() {
+        assertTrue(pointA.equals(pointD));
+        assertFalse(pointA.equals(pointB));
+        assertFalse(pointA.equals(pointC));
+    }
+
+    @Test
+    public void testGetters() {
+        assertEquals(1,pointA.getXVal());
+        assertEquals(2,pointA.getYVal());
+    }
+
+    @Test
+    public void testSetters() {
+        pointA.setXVal(7);
+        assertEquals(7,pointA.getXVal());
+
+        pointA.setYVal(6);
+        assertEquals(6,pointA.getYVal());
+    }
+
+
+
+}
